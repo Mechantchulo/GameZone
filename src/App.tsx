6ad86621fcb6e8a06d0780eec14ad735d61de0f8
Mechantchulo <chulomechant@gmail.com>
2025-07-02 18:23:53 +0300

    Removed code for ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 function App() {
   const items = ["Nairobi", "Mombasa", "Emali", "Makueni", "Kisumu"];
-  const handleSelectedItem = (item: string) => {
+  /*   const handleSelectedItem = (item: string) => {
     console.log(item);
-  };
+  }; */
   return (
     <div>
       {/*       <ListGroup
