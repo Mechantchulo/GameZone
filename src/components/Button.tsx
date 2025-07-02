e47272de160ce04cd24be9c040b77428c3422491
Mechantchulo <chulomechant@gmail.com>
2025-07-02 19:06:26 +0300

    updates

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,10 @@ interface ButtonProps {
 const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <>
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
         <strong>Holy guacamole!</strong> You should check in on some of those
         fields below.
         <button
