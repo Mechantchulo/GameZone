ad01cdbca55eb7134cc260986c3c3a3b3a72ff27
Mechantchulo <chulomechant@gmail.com>
2025-07-02 17:29:36 +0300

    Added useState and highlight event to click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,30 @@
-import { Fragment } from "react";
-
+import { MouseEvent, useState } from "react";
 function ListGroup() {
+  let items = ["Nairobi", "Mombasa", "Emali", "Makueni", "Kisumu"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    <Fragment>
+    <>
+      <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.length === 0 && <p>No item found</p>}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
