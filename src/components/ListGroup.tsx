1373610063390fdded75e4841b0cc7a4e22a33b3
Mechantchulo <chulomechant@gmail.com>
2025-07-02 18:08:15 +0300

    Passed props for printing selected item to console

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
 import { MouseEvent, useState } from "react";
-function ListGroup() {
-  let items = ["Nairobi", "Mombasa", "Emali", "Makueni", "Kisumu"];
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  //fucntion that prints the name of selected city
+  onSelectItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No item found</p>}
         {items.map((item, index) => (
@@ -18,6 +24,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
